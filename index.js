0ebf8a0bcf327f32b85a19bc64a1f6ed3a15da86
Zhanna Anikanova <whiteredfoxx@gmail.com>
2021-01-05 19:53:19 +0300

    feat:добавить функцию изменения профайла через попап

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close');
 let submitButton = document.querySelector('.popup__submit-button');
-
 
 function openPopup (){
     popup.classList.add('popup_opened');
@@ -18,15 +17,17 @@ function closePopup(){
     popup.classList.remove('popup_opened');
 }
 
-function editProfile(){
-    // let profileName = document.querySelector('.profile__name');
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    let profileName = document.querySelector('.profile__name');
     let inputName = document.querySelector('.popup__input_type_name');
-    // profileName.textContent = inputName.getAttribute('value');
-    console.log(inputName.textContent);
-    // console.log(profileName.textContent);
-    popup.classList.remove('popup_active');
+    profileName.textContent = inputName.value;
+    let profileProfession = document.querySelector('.profile__profession');
+    let inputProfession = document.querySelector('.popup__input_type_profession');
+    profileProfession.textContent = inputProfession.value;
+    popup.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-submitButton.addEventListener('click', editProfile);
+submitButton.addEventListener('click', handleFormSubmit);
